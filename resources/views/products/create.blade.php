a0148dfa8ee1da960c12417856026c0c7a8e1fc0
DanielPincu <ghepin01@easv365.dk>
2026-02-10 12:48:09 +0100

    Routes, Basic layouts, added

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,6 @@
+@extends('products.layout')
+@section('content')
+
+<h1 class="text-blue-400">Create New Product</h1>
+
+@endsection
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Management</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+<body>
+    <h1>Product Management</h1>
+    <div class="container mt-5">
+        <br>
+        @yield('content')
+    </div>
+
+
+</body>
+</html>
