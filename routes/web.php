847e75a2bbd94ada2271cf4af903c1db85817796
DanielPincu <ghepin01@easv365.dk>
2026-02-19 12:09:37 +0100

    Auth Added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Public pages
+Route::get('/', [ProductController::class, 'index']);
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+
+// Protected product actions
+Route::middleware('auth')->group(function () {
+    Route::resource('products', ProductController::class)->except(['index', 'show']);
 });
 
-Route::resource('products', ProductController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
